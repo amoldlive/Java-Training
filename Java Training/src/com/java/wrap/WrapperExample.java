efb8d728f8ab668f9fdf06bb185c5030484af890
amoldlive <32627412+amoldlive@users.noreply.github.com>
2022-04-20 19:58:48 +0530

    wrapper class
    wrapper class

--- Java Training/src/com/java/wrap/WrapperExample.java
+++ Java Training/src/com/java/wrap/WrapperExample.java
@@ -3,8 +3,45 @@ package com.java.wrap;
 public class WrapperExample {
 	public static void main(String[] args) {
 		
-		Integer i=10;
-	
+		byte b = 10; //primitive
+		Byte byteObj1=new Byte(b); //object //way-1
+		//OR
+		Byte byteObj2=Byte.valueOf(b); //way-2
+		//OR
+		Byte byteObj3=b; //internally java will use valueOf method
+		
+		Byte byteObj4=10; // Byte.valueOf(10);
+		System.out.println("Byte : "+byteObj3);
+		
+		short s = 11;
+		Short shortObj=Short.valueOf(s);
+		//OR
+		Short shortObj2=s;
+		System.out.println("Short : "+shortObj);
+		
+		int i = 12;
+		Integer intObj=Integer.valueOf(i);
+		System.out.println("Int : "+intObj);
+		
+		long l = 13L;
+		Long objLong=Long.valueOf(l);
+		System.out.println("Long : "+objLong);
+		
+		float f = 14.0F;
+		Float objFloat=Float.valueOf(f);
+		System.out.println("Float : "+objFloat);
+		
+		double d = 15;
+		Double objDouble=Double.valueOf(d);
+		System.out.println("Double : "+objDouble);
+		
+		char c = 'A';
+		Character objCHar=Character.valueOf(c);
+		System.out.println("CHar : "+objCHar);
+		
+		boolean isTrue = true;
+		Boolean booleanObj=Boolean.valueOf(isTrue);
+		System.out.println("boolean : "+booleanObj);
 		
 	}
 }
