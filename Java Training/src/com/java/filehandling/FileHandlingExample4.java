82b0a64f8f6352ffce2a632f3cdb1234699af9e3
amoldlive <32627412+amoldlive@users.noreply.github.com>
2022-04-27 18:46:10 +0530

    updating reference
    updating reference

--- Java Training/src/com/java/filehandling/FileHandlingExample4.java
+++ Java Training/src/com/java/filehandling/FileHandlingExample4.java
@@ -1,4 +1,4 @@
-package com.java.handlingfile;
+package com.java.filehandling;
 
 import java.io.File;
 import java.io.FileNotFoundException;
---
+++ Java Training/src/com/java/filehandling/FileHandlingExample7.java
@@ -0,0 +1,12 @@
+package com.java.filehandling;
+
+import java.io.IOException;
+
+public class FileHandlingExample7 {
+	public static void main(String[] args) throws IOException {
+
+		String filePath = "C:\\Users\\Amol D\\Desktop\\MyFolder\\Old\\Contact Details.txt";
+		
+		
+	}
+}
