a307be32b488279fe1868b4e549976b0a00314e3
amoldlive <32627412+amoldlive@users.noreply.github.com>
2022-05-31 09:05:38 +0530

    send redirect
    send redirect

---
+++ MyFirstWebApp/src/main/java/com/app/LoginServletV5.java
@@ -0,0 +1,25 @@
+package com.app;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import jakarta.servlet.RequestDispatcher;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+@WebServlet("/loginv5")
+public class LoginServletV5 extends HttpServlet {
+
+	@Override
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+
+		String userId = request.getParameter("uid");
+		String password = request.getParameter("pswd");
+
+		response.sendRedirect("loginv2?uid="+userId+"&pswd="+password);
+	}
+}
